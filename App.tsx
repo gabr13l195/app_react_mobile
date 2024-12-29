a93d76948f41e9461ecf183766abd20316723856
Wilmer Betancourt <wilmergabybet@gmail.com>
2024-12-29 11:13:30 -0500

    Segundo ejercicio finalizado

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Text } from 'react-native-paper';
 import { Cuadrado } from './ejercicios/01_cuadrado';
-import { arreglo } from './ejercicios/02_arreglo';
+import { Arreglos2 } from './ejercicios/02_arreglo';
 
 export const App = () => {
   return ( 
-    <Cuadrado/>
-    
+    // <Cuadrado/>
+    <Arreglos2/>
   )
 }
 
--- ejercicios/02_arreglo.tsx
+++ ejercicios/02_arreglo.tsx
@@ -1,9 +1,46 @@
-import React, { useState } from 'react';
-import { Text, View, StyleSheet, TextInput, Button, Alert } from 'react-native';
+// Desarrolle una función que reciba un arreglo como parámetro y como resultado retorne la suma de los 
+// elementos del arreglo
 
-export const arreglo = () => {
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+const sumarElementos = (arreglo: number[]): number => {
+    return arreglo.reduce((acumulador, valorActual) => acumulador + valorActual, 0);
+};
+
+export const Arreglos2 = () => {
+    const arregloEjemplo: number[] = [1, 2, 3, 4, 5];
+    const suma = sumarElementos(arregloEjemplo);
+
     return (
-        <View>
-            <Text></Text>
-        </View>    )
-}
+        <View style={styles.container}>
+            <Text style={styles.title}>Ejercicio #2 - Arreglos</Text>
+            <View>
+                <Text style={styles.text}>El arreglo es: {JSON.stringify(arregloEjemplo)}</Text>
+                <Text style={styles.text}>La suma de los elementos del arreglo es: {suma}</Text>
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#9B9FA6',
+        padding: 20,
+    },
+    title: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        marginBottom: 25,
+        color: "#FFFFFF"
+    },
+    text: {
+        fontSize: 18,
+        marginVertical: 10,
+        color: "#FFFFFF"
+    },
+});
+
